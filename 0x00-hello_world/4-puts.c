4164841fca085e1f04154c8e660e96127f862a7d
Ludwick Molebale <madimetjamolebale8@gmail.com>
2023-03-16 14:36:36 -0700

    Put troubel down

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main fuction for the program
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
