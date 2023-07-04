53c51d7343a67918097ecca979f2111aff30131e
Ludwick <madimetjamolebale8@gmail.com>
2023-07-04 14:50:15 -0700

    singly new

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints elements of singly linked list
+ *
+ * @t: passed list
+ * Return: returns count on success or -1 on fail
+ */
+
+size_t print_listint(const listint_t *t)
+{
+	size_t count = 0;
+	const listint_t *check;
+
+	if (t == NULL)
+	{
+		return (-1);
+	}
+	check = t;
+	while (check != NULL)
+	{
+		count += 1;
+		printf("%d\n", check->n);
+		check = check->next;
+	}
+	return (count);
+}
+
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,28 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * struct listint_t - structure for singly linked list
+ * @n: First member
+ *
+ * Description: structure for singly linked list named listint_t
+ */
+
+
+struct listint_t
+{
+    int n;
+    struct listint_t *next;
+};
+
+/**
+ * listint_t - Typedef for struc listint_t
+ *
+ */
+
+typedef struct listint_t listint_t;
+
+size_t print_listint(const listint_t *h);
+int _putchar(char c);
+
+#endif
