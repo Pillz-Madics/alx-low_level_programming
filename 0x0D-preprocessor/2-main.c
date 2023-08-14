30dd4c083baa49aa1cac42d3e00fffafe3c5b2dd
Ludwick <madimetjamolebale8@gmail.com>
2023-08-14 13:12:36 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function printing file name
+ *
+ * Return: return 0 on success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
